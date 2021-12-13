c0b3daf55eaa56055aceed51eeae624d090228fd
M. Rizky Azizi <ajijiren.alpha10@gmail.com>
2021-12-13 10:13:01 +0700

    desain halaman utama sipenmaru

---
+++ application/views/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sipenmaru | POLTEKKES KEMENKES PALEMBANG</title>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/assets/style/nav-style.css">
+    <link rel="stylesheet" href="<?= BASEURL; ?>/assets/style/index-style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="<?= BASEURL; ?>/assets/img/logo.jpg" alt="logo">
+            <div class="brand">
+                <h3>Penerimaan Mahasiswa Baru</h3>
+                <h3>Poltekkes Kemenkes Palembang</h3>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <h2>Pilih jalur Sipenmaru yang akan diikuti</h2>
+        <div class="container">
+            <div class="card">
+                <!-- <img src="" alt=""> -->
+                <div class="img"></div>
+                <p>Jalur penelusuran minat dan prestasi (PMDP)</p>          
+            </div>
+            <div class="card">
+                <!-- <img src="" alt=""> -->
+                <div class="img"></div>
+                <p>Jalur Keluarga Tidak Mampu secara Ekonomi (KTMSE) / Keluarga Miskin (GAKIN)</p>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>

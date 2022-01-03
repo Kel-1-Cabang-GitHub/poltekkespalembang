0a310bc439193e69935691c3d7bafdd63f1de719
NaufalK25 <naufalkateni2001@gmail.com>
2022-01-03 11:22:12 +0700

    6

--- assets/scripts/admin-script.js
+++ assets/scripts/admin-script.js
@@ -11,7 +11,7 @@ tombol_hapus1.forEach(el => {
 		let id = el.id;
 		let nama_lengkap = id.split(" ").slice(1).join(" ");
 		let link = id.split(" ")[0];
-		let nisn = link.split('/')[-1];
+		let nisn = link.split('/')[link.split('/') - 1].replace(/\D+/, '');
 		let span_nama = document.querySelector("span#ubah-nama");
 		let span_nisn = document.querySelector("span#ubah-nisn");
 		span_nama.textContent = nama_lengkap;
@@ -32,42 +32,22 @@ const foto_detail = document.querySelector("img#detail-foto");
 let detail_img = document.querySelector("img#detail-foto");
 let src_awal = detail_img.src;
 tombol_detail.forEach(el => {
-el.addEventListener("click", () => {
-	block.classList.add("active");
-	div_detail.classList.add("active");
-	let detail_data = data_obj[el.id];
-	Object.keys(detail_data).forEach(keys => {
-		if(keys === "pas_foto"){
-			detail_img = document.querySelector("img#detail-foto");
-			let src = detail_img.src.split("/");
-			src[src.length-3] = "uploads/img";
-			src[src.length-2] = "pas_foto"
-			src[src.length-1] = detail_data[keys];
-			src = src.join("/")
-			detail_img.src = src;
-		}else{
-			if(detail_data["jalur_pendaftaran"] === "KTMSE"){
-				let key = keys.split("_").map(el => el.charAt(0).toUpperCase()+el.slice(1)).join(" ");
-				let tr = document.createElement("tr");
-				let td_key = document.createElement("td");
-				let td_colon = document.createElement("td");
-				let td_values = document.createElement("td");
-				let key_text = document.createTextNode(key);
-				let colon_text = document.createTextNode(":");
-				let values_text = document.createTextNode("-");
-				if(detail_data[keys] !== null){
-					values_text = document.createTextNode(detail_data[keys]);
-				}
-				td_key.appendChild(key_text);
-				td_colon.appendChild(colon_text);
-				td_values.appendChild(values_text);
-				tr.appendChild(td_key);
-				tr.appendChild(td_colon);
-				tr.appendChild(td_values);
-				tabel_detail.appendChild(tr);
-			}else{
-				if(keys !== "surat_keterangan_miskin" && keys !== "surat_keterangan_penghasilan_keluarga" && keys !== "foto_rumah") {
-					let key = keys.split("_").map(el => el.charAt(0).toUpperCase()+el.slice(1)).join(" ");
+	el.addEventListener("click", () => {
+		block.classList.add("active");
+		div_detail.classList.add("active");
+		let detail_data = data_obj[el.id];
+		Object.keys(detail_data).forEach(keys => {
+			if (keys === "pas_foto") {
+				detail_img = document.querySelector("img#detail-foto");
+				let src = detail_img.src.split("/");
+				src[src.length - 3] = "uploads/img";
+				src[src.length - 2] = "pas_foto"
+				src[src.length - 1] = detail_data[keys];
+				src = src.join("/")
+				detail_img.src = src;
+			} else {
+				if (detail_data["jalur_pendaftaran"] === "KTMSE") {
+					let key = keys.split("_").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" ");
 					let tr = document.createElement("tr");
 					let td_key = document.createElement("td");
 					let td_colon = document.createElement("td");
@@ -75,7 +55,7 @@ el.addEventListener("click", () => {
 					let key_text = document.createTextNode(key);
 					let colon_text = document.createTextNode(":");
 					let values_text = document.createTextNode("-");
-					if(detail_data[keys] !== null){
+					if (detail_data[keys] !== null) {
 						values_text = document.createTextNode(detail_data[keys]);
 					}
 					td_key.appendChild(key_text);
@@ -85,18 +65,38 @@ el.addEventListener("click", () => {
 					tr.appendChild(td_colon);
 					tr.appendChild(td_values);
 					tabel_detail.appendChild(tr);
+				} else {
+					if (keys !== "surat_keterangan_miskin" && keys !== "surat_keterangan_penghasilan_keluarga" && keys !== "foto_rumah") {
+						let key = keys.split("_").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" ");
+						let tr = document.createElement("tr");
+						let td_key = document.createElement("td");
+						let td_colon = document.createElement("td");
+						let td_values = document.createElement("td");
+						let key_text = document.createTextNode(key);
+						let colon_text = document.createTextNode(":");
+						let values_text = document.createTextNode("-");
+						if (detail_data[keys] !== null) {
+							values_text = document.createTextNode(detail_data[keys]);
+						}
+						td_key.appendChild(key_text);
+						td_colon.appendChild(colon_text);
+						td_values.appendChild(values_text);
+						tr.appendChild(td_key);
+						tr.appendChild(td_colon);
+						tr.appendChild(td_values);
+						tabel_detail.appendChild(tr);
+					}
 				}
 			}
-		}
+		});
 	});
-});
 });
 
 //Tutup Modal
 const tombol_close = document.querySelector('div.close-btn a.detail');
-tombol_close.addEventListener("click",() => {
+tombol_close.addEventListener("click", () => {
 	div_detail.classList.remove("active");
-	while(tabel_detail.hasChildNodes()){
+	while (tabel_detail.hasChildNodes()) {
 		tabel_detail.removeChild(tabel_detail.lastChild);
 	}
 	detail_img.src = src_awal;

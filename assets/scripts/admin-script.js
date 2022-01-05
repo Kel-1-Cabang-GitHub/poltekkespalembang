3d8d1c7191dfd99306af7c1032e688db8be976ee
NaufalK25 <naufalkateni2001@gmail.com>
2022-01-05 10:51:17 +0700

    Redeploy

--- assets/scripts/admin-script.js
+++ assets/scripts/admin-script.js
@@ -44,59 +44,6 @@ tombol_detail.forEach(el => {
 		let detail_data = data_obj[el.id];
 		Object.keys(detail_data).forEach(keys => {
 			if (keys === "pas_foto") {
-<<<<<<< HEAD
-				detail_img = document.querySelector("img#detail-foto");
-				let src = detail_img.src.split("/");
-				src[src.length - 3] = "uploads/img";
-				src[src.length - 2] = "pas_foto"
-				src[src.length - 1] = detail_data[keys];
-				src = src.join("/")
-				detail_img.src = src;
-			} else {
-				if (detail_data["jalur_pendaftaran"] === "KTMSE") {
-					let key = keys.split("_").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" ");
-					let tr = document.createElement("tr");
-					let td_key = document.createElement("td");
-					let td_colon = document.createElement("td");
-					let td_values = document.createElement("td");
-					let key_text = document.createTextNode(key);
-					let colon_text = document.createTextNode(":");
-					let values_text = document.createTextNode("-");
-					if (detail_data[keys] !== null) {
-						values_text = document.createTextNode(detail_data[keys]);
-					}
-					td_key.appendChild(key_text);
-					td_colon.appendChild(colon_text);
-					td_values.appendChild(values_text);
-					tr.appendChild(td_key);
-					tr.appendChild(td_colon);
-					tr.appendChild(td_values);
-					tabel_detail.appendChild(tr);
-				} else {
-					if (keys !== "surat_keterangan_miskin" && keys !== "surat_keterangan_penghasilan_keluarga" && keys !== "foto_rumah") {
-						let key = keys.split("_").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" ");
-						let tr = document.createElement("tr");
-						let td_key = document.createElement("td");
-						let td_colon = document.createElement("td");
-						let td_values = document.createElement("td");
-						let key_text = document.createTextNode(key);
-						let colon_text = document.createTextNode(":");
-						let values_text = document.createTextNode("-");
-						if (detail_data[keys] !== null) {
-							values_text = document.createTextNode(detail_data[keys]);
-						}
-						td_key.appendChild(key_text);
-						td_colon.appendChild(colon_text);
-						td_values.appendChild(values_text);
-						tr.appendChild(td_key);
-						tr.appendChild(td_colon);
-						tr.appendChild(td_values);
-						tabel_detail.appendChild(tr);
-					}
-				}
-			}
-		});
-=======
 				let src = src_awal + detail_data[keys];
 				detail_img.src = src;
 			}
@@ -113,7 +60,6 @@ tombol_detail.forEach(el => {
 			};
 		});
 		div_data_detail.scrollTop = 0;
->>>>>>> b2db35e5cfe18f4a7d50e151c78b9bc890f7aecb
 	});
 });
 
@@ -121,12 +67,5 @@ tombol_detail.forEach(el => {
 const tombol_close = document.querySelector('div.close-btn a.detail');
 tombol_close.addEventListener("click", () => {
 	div_detail.classList.remove("active");
-<<<<<<< HEAD
-	while (tabel_detail.hasChildNodes()) {
-		tabel_detail.removeChild(tabel_detail.lastChild);
-	}
-	detail_img.src = src_awal;
-=======
->>>>>>> b2db35e5cfe18f4a7d50e151c78b9bc890f7aecb
 	block.classList.remove("active");
 });

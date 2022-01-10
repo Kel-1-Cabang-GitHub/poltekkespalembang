be5a70b91bc5250e5e04ef048608b3d21975ffa0
NaufalK25 <naufalkateni2001@gmail.com>
2022-01-10 08:57:53 +0700

    Change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/poltekkespalembang/';
+// $config['base_url'] = 'http://localhost:8080/poltekkespalembang/';
+$config['base_url'] = 'https://poltekkespalembang.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
